8b53fafd8f6d5f0b74daae0b6d6c08e74e193ef8
Yoojinjo <eugenejo111@gmail.com>
2024-07-26 14:48:23 -0500

    Part2 gallons of fuel

--- index.mjs
+++ index.mjs
@@ -64,3 +64,23 @@ console.log(a)
 // All numbers under 25
 const isUnder25 = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25;
 console.log(isUnder25)
+
+// PART 2
+// You are planning a cross-country road trip!
+// The distance of the trip, in total, is 1,500 miles.
+// Your car’s fuel efficiency is as follows:
+// At 55 miles per hour, you get 30 miles per gallon.
+// At 60 miles per hour, you get 28 miles per gallon.
+// At 75 miles per hour, you get 23 miles per gallon.
+// You have a fuel budget of $175.
+// The average cost of fuel is $3 per gallon.
+
+
+// Set up a program to answer the following questions:
+// How many gallons of fuel will you need for the entire trip?
+const FuelFiftyFive = 1500/30
+const FuelSixty = 1500/28
+const FuelSeventyFive = 1500/23
+
+// Will your budget be enough to cover the fuel expense?
+// How long will the trip take, in hours?
